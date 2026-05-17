4822e93d569312dceaf5a0880c2d1db1501e52f3
divyarawat09 <rawatdivya1505@gmail.com>
2026-05-18 01:58:26 +0530

    Updates
    Removed unused imports for joblib and re.

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import joblib
-import re
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 with open("index.html") as f:
